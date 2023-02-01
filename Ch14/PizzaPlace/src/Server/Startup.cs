feb19bdae0d372be51ff075b85da39158cbfbe99
Peter Himschoot <peter@u2u.be>
2023-02-01 12:51:45 +0100

    6.0.0

--- Ch14/PizzaPlace/src/Server/Startup.cs
+++ Ch14/PizzaPlace/src/Server/Startup.cs
@@ -34,6 +34,12 @@ namespace PizzaPlace.Server
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      using (var scope = app.ApplicationServices.CreateScope())
+      {
+        PizzaPlaceDbContext db = scope.ServiceProvider.GetRequiredService<PizzaPlaceDbContext>();
+        db.Database.EnsureCreated();
+      }
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
